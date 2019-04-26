0067f79aa482541986a7b4e8d316ee8d432c3050
Geoffrey Fielden <fielden.geoffrey@gmail.com>
2019-04-26 18:12:35 -0400

    Add data solution
    Add data solution and context file

---
+++ PetStoreAPI.Data/PetStoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace PetStoreAPI.Data
+{
+    public class PetStoreContext : DbContext
+    {
+        public PetStoreContext(DbContextOptions<PetStoreContext> options)
+             : base(options)
+        {
+        }
+
+    }
+}
---
+++ PetStoreAPI.Data/Repositories/AnimalRepository.cs
@@ -0,0 +1,43 @@
+﻿using PetStoreAPI.Domain;
+using PetStoreAPI.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PetStoreAPI.Data.Repositories
+{
+    public class AnimalRepository : IAnimalRepository
+    {
+        public Task<Animal> AddAsync(Animal animal, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(string id, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Animal>> GetAllAsync(CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Animal> GetByIdAsync(string id, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Animal animal, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
