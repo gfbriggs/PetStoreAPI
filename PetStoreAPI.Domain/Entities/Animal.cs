68c831c5f8797330d71a6e8aa4b954aed801ba8b
Geoffrey Fielden <fielden.geoffrey@gmail.com>
2019-04-25 22:38:56 -0400

    Added entities
    * Added the basic animal entities

--- PetStoreAPI.Domain/Entities/Animal.cs
+++ PetStoreAPI.Domain/Entities/Animal.cs
@@ -2,9 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace PetStoreAPI.Domain.Entities
+namespace PetStoreAPI.Domain
 {
     public class Animal
     {
+        public int AnimalId { get; set; }
+
+        public DateTimeOffset DateOfBirth { get; set; }
+
+        public double Price { get; set; }
+
+        public string Breed { get; set; }
+
+        public byte[] Picture { get; set; }
+
+        public AnimalType Type { get; set; }
     }
 }
---
+++ PetStoreAPI.Domain/Enums/AnimalType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetStoreAPI.Domain
+{
+    public enum AnimalType
+    {
+        Cat,
+        Dog,
+        Bird,
+        Reptile,
+        Fish
+    }
+}
