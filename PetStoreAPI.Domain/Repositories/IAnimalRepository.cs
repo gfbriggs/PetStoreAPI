25e23041c7352859026ff8a14b9bba38fbaa55c2
Geoffrey Fielden <fielden.geoffrey@gmail.com>
2019-04-25 23:02:25 -0400

    Changed controller
    Changed controllers to test routing and output

--- PetStoreAPI.Domain/Repositories/IAnimalRepository.cs
+++ PetStoreAPI.Domain/Repositories/IAnimalRepository.cs
@@ -1,4 +1,4 @@
-﻿using PetStoreAPI.Domain.Entities;
+﻿using PetStoreAPI.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
--- PetStoreAPI/Controllers/AnimalsController.cs
+++ PetStoreAPI/Controllers/AnimalsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PetStoreAPI.Domain;
 
 namespace PetStoreAPI.Controllers
 {
@@ -12,27 +13,39 @@ namespace PetStoreAPI.Controllers
     {
         // GET api/animals
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        [Produces(typeof(List<Animal>))]
+        public async Task<ActionResult<IEnumerable<Animal>>> Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                // TODO : Replace with real repository call
+
+                return new ObjectResult(new List<Animal>() { new Animal() { AnimalId = 1, Breed = "Tabby", Type = AnimalType.Cat } });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e);
+            }
         }
 
         // GET api/animals/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        [Produces(typeof(Animal))]
+        public async Task<ActionResult<Animal>> Get(int id)
         {
-            return "value";
+            // TODO : Replace with real repository call
+            return new ObjectResult(new Animal() { AnimalId = 2, Breed = "Rex", Type = AnimalType.Cat });
         }
 
         // POST api/animals
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Animal value)
         {
         }
 
         // PUT api/animals/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Animal value)
         {
         }
 
