4071677c3d52b151fc9e84a25c4fc11da174d09e
Geoffrey Fielden <fielden.geoffrey@gmail.com>
2019-05-09 20:14:58 -0400

    Added some crud operations
    Added more operations to the animal repo

--- PetStoreAPI.Data/Repositories/AnimalRepository.cs
+++ PetStoreAPI.Data/Repositories/AnimalRepository.cs
@@ -1,4 +1,5 @@
-﻿using PetStoreAPI.Domain;
+﻿using Microsoft.EntityFrameworkCore;
+using PetStoreAPI.Domain;
 using PetStoreAPI.Domain.Repositories;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,16 @@ namespace PetStoreAPI.Data.Repositories
 {
     public class AnimalRepository : IAnimalRepository
     {
+
+        private readonly PetStoreContext _context;
+
+        public AnimalRepository(PetStoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AnimalExists(string id, CancellationToken ct = default) => await _context.Animals.FindAsync(id) != null;
+
         public async Task<Animal> AddAsync(Animal animal, CancellationToken ct = default)
         {
             throw new NotImplementedException();
@@ -16,22 +27,32 @@ namespace PetStoreAPI.Data.Repositories
 
         public async Task<bool> DeleteAsync(string id, CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            if (await AnimalExists(id, ct))
+            {
+                _context.Remove(await _context.Animals.FindAsync(id, ct));
+
+                await _context.SaveChangesAsync(ct);
+
+                return true;
+            }
+            return false;
         }
 
         public void Dispose()
         {
             throw new NotImplementedException();
         }
-        
+
         public async Task<List<Animal>> GetAllAsync(CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            var animals = _context.Animals;
+
+            return await animals.ToListAsync(ct);
         }
 
         public async Task<Animal> GetByIdAsync(string id, CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            return await _context.Animals.FindAsync(id, ct);
         }
 
         public async Task<bool> UpdateAsync(Animal animal, CancellationToken ct = default)
